cc21b63434d8dee0d9fc78517a6f63cbb5cd2fc4
faelpatrick <faelpatrick@gmail.com>
2022-12-31 14:10:08 +0000

    ajustes

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -16,15 +16,27 @@ import StudantPage from "./pages/StudantPages/StudantPage";
 const AppRoutes = () => {
     return (
 
-        <Router>
+        // <Router>
+        //     <Routes>
+        //         <Route  path="/" exact element={<MainPage />} />
+        //         <Route  path="/uni-crud" exact element={<MainPage />} />
+        //         <Route  path="/cursos" exact element={<CoursesPage />} />
+        //         <Route  path="/disciplinas" exact element={<DisciplinasPage />} />
+        //         <Route  path="/professores" exact element={<ProfessorPage />} />
+        //         <Route  path="/alunos" exact element={<StudantPage />} />
+        //         <Route exact path="/login" element={<LoginPage />} />
+        //     </Routes>
+        // </Router>
+
+           <Router>
             <Routes>
-                <Route  path="/" exact element={<MainPage />} />
-                <Route  path="/uni-crud" exact element={<MainPage />} />
-                <Route  path="/cursos" exact element={<CoursesPage />} />
-                <Route  path="/disciplinas" exact element={<DisciplinasPage />} />
-                <Route  path="/professores" exact element={<ProfessorPage />} />
-                <Route  path="/alunos" exact element={<StudantPage />} />
-                <Route exact path="/login" element={<LoginPage />} />
+                <Route  path="/"  element={<MainPage />} />
+                <Route  path="/uni-crud"  element={<MainPage />} />
+                <Route  path="/cursos"  element={<CoursesPage />} />
+                <Route  path="/disciplinas"  element={<DisciplinasPage />} />
+                <Route  path="/professores"  element={<ProfessorPage />} />
+                <Route  path="/alunos"  element={<StudantPage />} />
+                <Route  path="/login" element={<LoginPage />} />
             </Routes>
         </Router>
     );
